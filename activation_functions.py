8ccb950c577607d256bc93670ba21f9e434f5ed7
Mahmoud Sayed <96210654+MahmoudSayed216@users.noreply.github.com>
2024-03-09 16:07:06 +0200

    activation functions

---
+++ activation_functions.py
@@ -0,0 +1,13 @@
+EULER_CONSTANT = 2.7182818
+
+Identity = lambda y : y
+
+ReLU = lambda y : y if y >= 0 else 0
+
+leaky_ReLU = lambda y : y if y >= 0 else 0.1*y
+
+sigmoid = lambda y : 1 / (1 + EULER_CONSTANT ** -y)
+
+signum = lambda y : 1 if y >= 0 else 0
+
+tanh = lambda y, a : 1 - EULER_CONSTANT**(-y*a) / 1 + EULER_CONSTANT**(-y*a)   
