f3e0474848813b5259aa683b83057a6c8c62a11a
Mahmoud Sayed <96210654+MahmoudSayed216@users.noreply.github.com>
2024-03-10 19:49:51 +0300

    Update activation_functions.py

--- activation_functions.py
+++ activation_functions.py
@@ -1,4 +1,6 @@
-EULER_CONSTANT = 2.7182818
+import numpy as np 
+from math import e
+
 
 identity = lambda y : y
 
@@ -6,8 +8,21 @@ ReLU = lambda y : y if y >= 0 else 0
 
 leaky_ReLU = lambda y : y if y >= 0 else 0.1*y
 
-sigmoid = lambda y : 1 / (1 + EULER_CONSTANT ** -y)
+sigmoid = lambda y : 1 / (1 + e ** -y)
 
-signum = lambda y : 1 if y >= 0 else 0
+signum = lambda y : 1 if y >= 0 else -1
 
-tanh = lambda y, a : (1 - EULER_CONSTANT**(-y*a)) / (1 + EULER_CONSTANT**(-y*a))   
+tanh = lambda y, a : (1 - e**(-y*a)) / (1 + e**(-y*a))   
+
+
+d_identity = lambda y : 1
+
+d_ReLU = lambda y : 1 if y >= 0 else 0
+
+d_leaky_Relu = lambda y : 1 if y >= 0 else 0.1
+
+d_sigmoid = lambda y : sigmoid(y) * (1 - sigmoid(y))
+
+d_signum = lambda y : 0
+
+d_tanh = lambda y, a: 2*a*((e**(-y*a)) / (1 + e**(-y*a))**2)
