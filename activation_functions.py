aa4f8683b55d46b5d75282052333b3d24fb3a32a
Mahmoud Sayed <96210654+MahmoudSayed216@users.noreply.github.com>
2024-03-10 19:52:43 +0300

    Update activation_functions.py

--- activation_functions.py
+++ activation_functions.py
@@ -2,27 +2,27 @@ import numpy as np
 from math import e
 
 
-identity = lambda y : y
+Identity = lambda y : y
 
 ReLU = lambda y : y if y >= 0 else 0
 
-leaky_ReLU = lambda y : y if y >= 0 else 0.1*y
+Leaky_ReLU = lambda y : y if y >= 0 else 0.1*y
 
-sigmoid = lambda y : 1 / (1 + e ** -y)
+Sigmoid = lambda y : 1 / (1 + e ** -y)
 
-signum = lambda y : 1 if y >= 0 else -1
+Signum = lambda y : 1 if y >= 0 else -1
 
-tanh = lambda y, a : (1 - e**(-y*a)) / (1 + e**(-y*a))   
+Tanh = lambda y, a : (1 - e**(-y*a)) / (1 + e**(-y*a))   
 
 
-d_identity = lambda y : 1
+d_Identity = lambda y : 1
 
 d_ReLU = lambda y : 1 if y >= 0 else 0
 
-d_leaky_Relu = lambda y : 1 if y >= 0 else 0.1
+d_Leaky_ReLU = lambda y : 1 if y >= 0 else 0.1
 
-d_sigmoid = lambda y : sigmoid(y) * (1 - sigmoid(y))
+d_Sigmoid = lambda y : sigmoid(y) * (1 - sigmoid(y))
 
-d_signum = lambda y : 0
+d_Signum = lambda y : 0
 
-d_tanh = lambda y, a: 2*a*((e**(-y*a)) / (1 + e**(-y*a))**2)
+d_Tanh = lambda y, a: 2*a*((e**(-y*a)) / (1 + e**(-y*a))**2)
