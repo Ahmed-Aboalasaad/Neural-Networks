55c5a18c5276b550e98fda28879275ce49e431ae
Mahmoud Sayed <96210654+MahmoudSayed216@users.noreply.github.com>
2024-03-11 15:54:52 +0300

    Update Adaline.py

--- Adaline.py
+++ Adaline.py
@@ -1,45 +1,43 @@
 import numpy as np
+import pandas as pd
+import random
+import activation_functions
 
-EPSILON = 0.0001
+def mean_squared_error(y_true, y_pred):
+    return np.sum((y_true - y_pred)**2) / len(y_true)
 
-def mean_square_error(y_true, y_pred):
-        m = y_true.shape[0]
-        diff_squared = (y_true - y_pred)**2
-        mse = diff_squared.sum() / m
-        return mse
 
 class Adaline:
-    def __init__(self, learning_rate) -> None:
+    def __init__(self, learning_rate, activation_function, EPSILON) -> None:
         self.learning_rate = learning_rate
-        self.w_0 = 0 #bias term
-        self.w_1 = 0
-        self.w_2 = 0
+        self.activation_function = activation_function
+        self.weights = np.array([[0.001], [0.03]])
+        self.bias = random.random()
+        self.EPSILON = EPSILON
 
 
-    def fit(self, x_train, y_train):
-        predictions = self.predict(x_train)
-        print(type(predictions))
-        print(type(y_train))
-        prev_MSE = mean_square_error(y_train, predictions)
+    def update_weights(self, total_error, X_train):
+            self.weights[0] = self.weights[0] + ((self.learning_rate*total_error)*X_train[:, 0]).sum()
+            self.weights[1] = self.weights[1] + ((self.learning_rate*total_error)*X_train[:, 1]).sum()
+            self.bias = self.bias + self.learning_rate*total_error
+
+            
+    def fit(self, X_train, y_train) -> None:
+        predictions = self.predict(X_train)
+        prev_MSE = mean_squared_error(y_train, predictions)
         curr_MSE = -1e20
-        while abs(prev_MSE - curr_MSE) > EPSILON:
-            for x1, x2, t in zip(x_train[:, 0], x_train[:, 1], y_train[:]):
-                y = self.predict_single(x1, x2)
-                error = t - y
-                self.w_0 = self.w_0 + self.update_weight_with_GD(error, 1)
-                self.w_1 = self.w_1 + self.update_weight_with_GD(error, x1)
-                self.w_2 = self.w_2 + self.update_weight_with_GD(error, x2)
+        while abs(prev_MSE - curr_MSE) > self.EPSILON:
+            total_error = np.sum((y_train - predictions))
+            self.update_weights(total_error, X_train)
+            predictions = self.predict(X_train)
+            prev_MSE = curr_MSE
+            curr_MSE = mean_squared_error(y_train, predictions)
 
-            predictions = self.predict(x_train)
-            prev_MSE = mean_square_error(y_train, predictions)
-        
-    def update_weight_with_GD(self, error, x):
-        return self.learning_rate * error * x
+    def predict(self, X_test : np.ndarray) -> np.ndarray:
+        y = X_test.dot(self.weights) + self.bias
+        a = np.vectorize(self.activation_function)(y)
+        return a
 
-
-    def predict(self, X_test):
-        x_w = X_test[:, 0]*self.w_1 + X_test[:, 1] * self.w_2 + self.w_0
-        return x_w
-    
     def predict_single(self, x1, x2):
         return x1*self.w_1 + x2*self.w_2 + self.w_0
+    
