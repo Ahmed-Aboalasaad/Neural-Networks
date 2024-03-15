7cb0d3fd391c685a26edb4bf99942563e6c65796
Mahmoud Sayed <96210654+MahmoudSayed216@users.noreply.github.com>
2024-03-15 12:55:23 +0300

    Update activation_functions.py

--- activation_functions.py
+++ activation_functions.py
@@ -2,11 +2,11 @@ import numpy as np
 from math import e
 
 
-Identity = lambda y : y
+identity = lambda y : y
 
 ReLU = lambda y : y if y >= 0 else 0
 
-Leaky_ReLU = lambda y : y if y >= 0 else 0.1*y
+leaky_ReLU = lambda y : y if y >= 0 else 0.1*y
 
 Sigmoid = lambda y : 1 / (1 + e ** -y)
 
@@ -15,13 +15,13 @@ Signum = lambda y : 1 if y >= 0 else -1
 Tanh = lambda y, a : (1 - e**(-y*a)) / (1 + e**(-y*a))   
 
 
-d_Identity = lambda y : 1
+d_identity = lambda y : 1
 
 d_ReLU = lambda y : 1 if y >= 0 else 0
 
-d_Leaky_ReLU = lambda y : 1 if y >= 0 else 0.1
+d_leaky_Relu = lambda y : 1 if y >= 0 else 0.1
 
-d_Sigmoid = lambda y : sigmoid(y) * (1 - sigmoid(y))
+d_Sigmoid = lambda y : Sigmoid(y) * (1 - Sigmoid(y))
 
 d_Signum = lambda y : 0
 
